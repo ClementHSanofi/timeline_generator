f4bc04fa0302c25dcb153d919b7fdd793d04df92
clement hauguel <clement.hauguel@sanofi.com>
2026-06-22 11:38:07 +0200

    📦 NEW: Implement export to png feature

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 import { initiateForm, autoRenderTimeline, loadFormData} from "./form.js";
 import { initSideform } from "./sideform.js";
 import { fakeData } from "./fakeData.js";
-import { exportTimelineAsPDF } from "./pdf.js";
+import { exportTimelineAsPDF, exportTimelineAsPNG } from "./exportTimeline.js";
 import { exportFormData, importFormData } from "./export-import.js";
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -12,7 +12,14 @@ document.addEventListener("DOMContentLoaded", () => {
     // Trigger one auto render after loading demo data
     document.getElementById("event-form").dispatchEvent(new Event("input", { bubbles: true }));
     initSideform();
-    document.getElementById("export-pdf-btn").addEventListener("click", exportTimelineAsPDF);
+    document.getElementById("export-btn").addEventListener("click", () => {
+        const format = document.getElementById("export-format").value;
+        if (format === "png") {
+            exportTimelineAsPNG();
+            return;
+        }
+        exportTimelineAsPDF();
+    });
     document.getElementById("export-json-btn").addEventListener("click", exportFormData);
     document.getElementById("import-json-btn").addEventListener("click", importFormData);
 });
